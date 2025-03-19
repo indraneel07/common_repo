f95224a5270fbe32fb2390c7b8b883a105fddb99
indraneeldongaonkar <indraneel.dongaonkar@astronomer.io>
2025-03-19 18:21:58 +0530

    Modified DAG

--- dags/exampledag_common.py
+++ dags/exampledag_common.py
@@ -35,7 +35,7 @@ import requests
     default_args={"owner": "Astro", "retries": 3},
     tags=["example"],
 )
-def example_astronauts():
+def example_common_astronauts():
     # Define tasks
     @task(
         # Define a dataset outlet for the task. This can be used to schedule downstream DAGs when this task has run.
@@ -97,4 +97,4 @@ def example_astronauts():
 
 
 # Instantiate the DAG
-example_astronauts()
+example_common_astronauts()
